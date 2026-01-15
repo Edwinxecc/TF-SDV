7e27def824bb6ca9a14fc7c20ad0a23bd4fc5602
Edwin <caizadavid417@gmail.com>
2026-01-14 21:30:59 -0500

    add basic GUI

--- src/main/java/ec/edu/uce/gui/Test.java
+++ src/main/java/ec/edu/uce/gui/Test.java
@@ -1,4 +1,17 @@
 package ec.edu.uce.gui;
 
+import javax.swing.*;
+
 public class Test {
+    private JList list1;
+    private JButton lectorDeBarrasButton;
+    private JButton codigoDeBarrasButton;
+    private JTextField Barras;
+    private JButton button2;
+    private JButton pagoTargetaButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button1;
+    private JButton eiminarButton;
+    private JTextField textField3;
 }
